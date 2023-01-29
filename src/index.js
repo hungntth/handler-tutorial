96a4ff499d56a6ccaca8fff5877705e5670678e0
HungNt <hungnt@techasians.com>
2023-01-29 21:24:21 +0700

    base code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const {engine} = require('express-handlebars');
+const router = require('./routes/index');
+const path = require('path');
 require('dotenv').config();
 
 const port = process.env.PORT;
 const versionNode = process.versions.node;
+
+app.engine('.hbs',engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.dirname(__dirname)+'/src/views');
+
+app.use(router)
 
 app.listen(port, () => {
     console.log(`server run http://localhost:${port}`);
