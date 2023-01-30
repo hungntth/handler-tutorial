81cd0f58260b876853db4cb88d1492d17eeb2ff5
hungntth <1119thaihungnguyen@gmail.com>
2023-01-30 13:22:10 +0700

    data news

---
+++ src/crawl/news.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const getNews = async () => {
+  try {
+    const siteUrl = "https://lienminh.vnggames.com/vi-vn/latest-news/";
+    const { data } = await axios({
+      method: "GET",
+      url: siteUrl,
+    });
+    const itemArr = [];
+    const test = cheerio.load(data);
+    const elementSelector = `#blt0dc6123acc9a8152-section_1 > div > div > div.style__Wrapper-sc-106zuld-0.style__ResponsiveWrapper-sc-106zuld-4.dRXdjM.kXYoSI.style__List-sc-1ynvx8h-3.cwQJeW > div > ol > li`;
+    test(elementSelector).each((parentIdx, parentElm) => {
+      const id = parentIdx + 1;
+      let link = test(parentElm).find("a");
+      link = link[0].attribs;
+      if (link["href"].search("https://www") === -1) {
+        link = "https://lienminh.vnggames.com" + link["href"];
+      } else {
+        link = link["href"];
+      }
+      const title = test(parentElm)
+        .find("a > article > div.style__Info-sc-1h41bzo-6.evyNIL > div > h2")
+        .text();
+      let image = test(parentElm).find(
+        "a > article > div.style__Image-sc-1h41bzo-2.cbzxFR > div > div > img"
+      );
+      image = image[0].attribs["src"];
+      itemArr.push({ id, title, link, image });
+    });
+    console.log(itemArr);
+    return itemArr;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getNews();
