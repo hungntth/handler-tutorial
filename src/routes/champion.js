0a415a106e475df507910ef4933468eb407d7d40
hungntth <1119thaihungnguyen@gmail.com>
2023-01-30 13:25:01 +0700

    data news

--- src/routes/champion.js
+++ src/routes/champion.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { getAllChamp } = require("../controller/championController");
-const championRouter = require("./champion");
 
-router.use("/", championRouter);
+router.get("/", (req, res) => {
+  return getAllChamp(req, res);
+});
+
+router.get("/getChamp/:id", (req, res) => {
+  res.render("detail");
+});
 
 module.exports = router;
