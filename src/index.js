fecbe7298e47519f53777c5481d09fe41c322899
hungntth <1119thaihungnguyen@gmail.com>
2023-02-10 15:31:29 +0700

    socket

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const { engine } = require("express-handlebars");
 const router = require("./routes/index");
 const path = require("path");
 require("dotenv").config();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const port = process.env.PORT;
 const versionNode = process.versions.node;
@@ -14,7 +18,14 @@ app.set("views", path.dirname(__dirname) + "/src/views");
 
 app.use(router);
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  socket.on('on-chat', data => {
+    io.emit('user-chat', data)
+  })
+});
+
+
+server.listen(port, () => {
   console.log(`server run http://localhost:${port}`);
   console.log(`With version node ${versionNode}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const championRouter = require("./champion");
 const newsRouter = require("./news");
 const detailRouter = require("./detail");
+const chat = require("./chat");
 
 router.use("/", championRouter);
 router.use("/getChamp", detailRouter);
 router.use("/news", newsRouter);
+router.use("/chat", chat);
 
 module.exports = router;
